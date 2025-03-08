df18e151cdc4cb9f1a53b1329019fe7ff5d19f86
Vansh Srivastava <153951186+vanxxhuu10@users.noreply.github.com>
2025-03-08 08:17:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function analyzeEmail() {
     }
 
     try {
-        const response = await fetch("http://localhost:5000/analyze-email", {
+        const response = await fetch("https://phishing-detector-l8ce.onrender.com/predict", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ email: emailText })  // Make sure this matches backend key
